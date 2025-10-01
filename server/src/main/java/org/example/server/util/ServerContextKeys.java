5b3162aab63ce8645267dee9906f338dfea0bcbf
peyman <peyman.alad@gmail.com>
2025-10-01 22:56:17 +0330

    change ServerContextKeys.java

--- server/src/main/java/org/example/server/util/ServerContextKeys.java
+++ server/src/main/java/org/example/server/util/ServerContextKeys.java
@@ -4,8 +4,18 @@ package org.example.server.util;
  * Keys used to store data in the jPOS transaction {@link org.jpos.transaction.Context}.
  */
 public final class ServerContextKeys {
-    public static final String REQUEST = "request";
-    public static final String RESPONSE = "response";
+    /**
+     * jPOS stores the inbound {@link org.jpos.iso.ISOMsg} under the upper-case
+     * key {@code REQUEST}. The previous lowercase value caused transaction
+     * participants to miss the ISO message and prevented QMUX from finding the
+     * response produced by {@link org.example.server.participant.BuildResponse}.
+     */
+    public static final String REQUEST = "REQUEST";
+    /**
+     * TMUX expects the response message to be available under the upper-case
+     * {@code RESPONSE} key before it can write it back to the network channel.
+     */
+    public static final String RESPONSE = "RESPONSE";
     public static final String RESPONSE_CODE = "responseCode";
     public static final String ERROR_MESSAGE = "errorMessage";
     public static final String APPROVAL_CODE = "approvalCode";
